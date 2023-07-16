51c6b68977639d61bb6da5d861daefff0b5e622d
lhelmi <7m.helmi@gmail.com>
2023-07-16 15:47:25 +0700

    revisi hal depan

--- resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/navbar.blade.php
@@ -23,9 +23,11 @@
         @each('adminlte::partials.navbar.menu-item', $adminlte->menu('navbar-right'), 'item')
 
         {{-- User menu link --}}
-        @include('admin.layout.navbar.home')
         @if(Auth::user())
             @if(config('adminlte.usermenu_enabled'))
+                @if(Auth::user()->role == '0')
+                    @include('admin.layout.navbar.home')
+                @endif
                 @include('adminlte::partials.navbar.menu-item-dropdown-user-menu')
             @else
                 @include('adminlte::partials.navbar.menu-item-logout-link')
