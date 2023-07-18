de779117801b95f3f1aee699e9dada3f62df1c2a
lhelmi <7m.helmi@gmail.com>
2023-07-18 11:17:35 +0700

    revisi pemesanan/order

--- resources/views/front/order.blade.php
+++ resources/views/front/order.blade.php
@@ -5,13 +5,13 @@
         <div class="content-1">
             <h1 class="jumbotron-heading">Hubungi</h1>
             <img src="{{ asset('front/images/Wa-Icon.png') }}" style="height: 45px; width: 48px;" data-holder-rendered="true" >
-            <strong class="text-gray-dark">0812947593759</strong>
+            <strong class="text-gray-dark">081219450221</strong>
             <br>
             <img src="{{ asset('front/images/Wa-Icon.png') }}" style="height: 45px; width: 48px;" data-holder-rendered="true" >
-            <strong class="text-gray-dark">0812947593759</strong>
+            <strong class="text-gray-dark">085293444755</strong>
             <br>
             <img src="{{ asset('front/images/Wa-Icon.png') }}" style="height: 45px; width: 48px;" data-holder-rendered="true" >
-            <strong class="text-gray-dark">0812947593759</strong>
+            <strong class="text-gray-dark">082214008859</strong>
         </div>
         <div class="content-2">
             <h1 class="jumbotron-heading">Media Sosial</h1>
