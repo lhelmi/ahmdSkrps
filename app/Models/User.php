8d12ca66cadac76217ff987b133ee176a4e9aebf
lhelmi <7m.helmi@gmail.com>
2023-07-20 08:07:54 +0700

    revisi user delete soft

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,16 +9,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'name',
         'email',
